fa1c46cafeee3930a7935a27f4e760202e3b45b8
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-06 18:03:28 +0200

    Set Home routing

--- dev/src/app/routes/home.routing.ts
+++ dev/src/app/routes/home.routing.ts
@@ -6,5 +6,25 @@ export const HOME_ROUTING: Route[] = [
     path: '',
     pathMatch: 'full',
     component: PresentationComponent
+  },
+  {
+    path: 'skills',
+    pathMatch: 'full',
+    component: SkillsComponent
+  },
+  {
+    path: 'education',
+    pathMatch: 'full',
+    component: EducationComponent
+  },
+  {
+    path: 'experience',
+    pathMatch: 'full',
+    component: ExperienceComponent
+  },
+  {
+    path: 'contact',
+    pathMatch: 'full',
+    component: ContactComponent
   }
 ];
