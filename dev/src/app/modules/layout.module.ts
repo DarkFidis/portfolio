9762f56da0110c4d473708abfa181a38b17caeb6
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-08 19:42:58 +0200

    WIP Install Font-Awesome

--- dev/src/app/modules/layout.module.ts
+++ dev/src/app/modules/layout.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const MODULES = [
   CommonModule,
   FlexLayoutModule,
-  MaterialModule,
-  FontAwesomeModule
+  MaterialModule
 ]
 
 @NgModule({
