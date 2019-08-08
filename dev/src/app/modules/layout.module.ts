70f937727bb0af75b9456a3e96ca7ffc1a73f54f
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-08 18:45:43 +0200

    Add FontAwesomeModule to app

--- dev/src/app/modules/layout.module.ts
+++ dev/src/app/modules/layout.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const MODULES = [
   CommonModule,
   FlexLayoutModule,
-  MaterialModule
+  MaterialModule,
+  FontAwesomeModule
 ]
 
 @NgModule({
