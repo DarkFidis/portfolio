8d2422385ce8758b5eda5f1602ecdddd428d76dc
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-13 17:40:51 +0200

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const multer = require('multer');
+
+app.listen(3000);
