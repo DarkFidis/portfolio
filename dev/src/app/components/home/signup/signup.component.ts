4525f9d6cd45d41b4672e265caf136b5c8670c75
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-13 20:09:45 +0200

    Create User model

--- dev/src/app/components/home/signup/signup.component.ts
+++ dev/src/app/components/home/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-signup',
@@ -12,7 +14,8 @@ export class SignupComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit() {
