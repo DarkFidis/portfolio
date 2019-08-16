7074c887875b2f98e06be9aaf8607504a174c2a3
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-16 20:30:32 +0200

    Add Token handling

--- dev/src/app/services/auth.service.ts
+++ dev/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject, timer, of, Subscription } from 'rxjs';
+import { tap, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../models/user.model';
@@ -13,6 +13,7 @@ export class AuthService {
 
   constructor(private http: HttpClient) {
     this.initToken();
+    this.subscription = this.initTimer();
   }
 
   public jwtToken: BehaviorSubject<JwtToken> = new BehaviorSubject({
@@ -35,6 +36,34 @@ export class AuthService {
     }
   }
 
+  public initTimer() {
+    return timer(2000, 100000).pipe(
+      switchMap(() => {
+        if(localStorage.getItem('jwt')) {
+          return this.http.get<string>('/api/auth/refresh-token').pipe(
+            tap((token: string) => {
+              this.jwtToken.next({
+                isAuth: true,
+                token: token
+              });
+              localStorage.setItem('jwt', token);
+            })
+          );
+        } else {
+          this.subscription.unsubscribe();
+          return of(null);
+        }
+      })
+    ).subscribe(() => {}, err => {
+      this.jwtToken.next({
+        isAuth: false,
+        token: null
+      });
+      localStorage.removeItem('jwt');
+      this.subscription.unsubscribe();
+    });
+  }
+
   public signup(user: User): Observable<User> {
     return this.http.post<User>('/api/auth/signup', user);
   }
@@ -47,6 +76,7 @@ export class AuthService {
           token: token
         });
         localStorage.setItem('jwt', token);
+        this.subscription = this.initTimer();
       })
     )
   }
