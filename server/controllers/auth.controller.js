8bfb877ebd9c323d30be88a6b5697e731ec390c4
Matthieu Seynaeve <matthieu.seynaeve@free.fr>
2019-08-16 19:30:32 +0200

    Add isLoggedIn method

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
-const RSA_KEY_PRIVATE = fs.readFileSync('./rsa/key')
+const RSA_KEY_PRIVATE = fs.readFileSync('./rsa/key');
+const RSA_PUBLIC_KEY = fs.readFileSync('./rsa/key.pub');
 
 exports.signup = (req, res, next) => {
   console.log('Signup-ing')
@@ -34,4 +35,23 @@ exports.signin = (req, res, next) => {
       res.status(401).json('Erreur de connexion');
     }
   })
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  const token = req.headers.authorization;
+  if(token) {
+    jwt.verify(token, RSA_PUBLIC_KEY, (err, decoded) => {
+      if(err) {
+        return res.status(401).json('Token invalide');
+      }
+      const sub = decoded.sub;
+      User.findOne({ '_id': sub}).exec( (err, user) => {
+        if(err || !user) {
+          res.status(401).json('Erreur')
+        }
+        req.user = user;
+        next();
+      })
+    })
+  }
 }
